a7c6ca8f142d522578826e22a9fff5e5a282f18c
jtradwil <jtradwil@mtu.edu>
2017-01-11 13:30:01 -0800

    completed tutorial 1

---
+++ jtradwil/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/src
+# Build directory: /home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
