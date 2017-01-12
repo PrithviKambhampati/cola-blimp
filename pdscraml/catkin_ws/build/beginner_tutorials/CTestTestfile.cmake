b0b9d417991f4db6b8c8a47f3ba728f7d1b402e9
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-01-12 13:19:22 -0800

    Tutorial 3

--- pdscraml/catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
+++ pdscraml/catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/robotics/catkin_ws/src
-# Build directory: /home/robotics/catkin_ws/build
+# Source directory: /home/robotics/catkin_ws/src/beginner_tutorials
+# Build directory: /home/robotics/catkin_ws/build/beginner_tutorials
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(beginner_tutorials)
