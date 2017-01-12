2e2b2b2627fbd257c1082d4e2d16c72419655027
jtradwil <jtradwil@mtu.edu>
2017-01-12 12:27:28 -0800

    completed tutorial 11

---
+++ jtradwil/catkin_ws/build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
