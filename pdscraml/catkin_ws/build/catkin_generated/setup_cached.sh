28c24f13450c71f93cc6fd83ecffb6952c05e88c
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-01-12 13:52:23 -0800

    Tutorial 4

--- pdscraml/catkin_ws/build/catkin_generated/setup_cached.sh
+++ pdscraml/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -9,6 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/robotics/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/robotics/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/robotics/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/robotics/catkin_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/robotics/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/robotics/catkin_ws/src:$ROS_PACKAGE_PATH"
