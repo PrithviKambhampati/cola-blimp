f05629086ca72399a694714d1f97cc519aee9b4f
jtradwil <jtradwil@mtu.edu>
2017-01-12 12:16:35 -0800

    competed tutorial 10

---
+++ jtradwil/catkin_ws/build/beginner_tutorials/catkin_generated/beginner_tutorials-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/src/beginner_tutorials/msg/Num.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
--- jtradwil/catkin_ws/build/catkin_generated/setup_cached.sh
+++ jtradwil/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/src:$ROS_PACKAGE_PATH"
