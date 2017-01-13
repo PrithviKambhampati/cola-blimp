3d0cb6d34c2f88d89a4791a110be6e153fb180ac
jtradwil <jtradwil@mtu.edu>
2017-01-13 07:52:20 -0800

    completed tutorial 14

--- jtradwil/catkin_ws/build/catkin_generated/setup_cached.sh
+++ jtradwil/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CPATH="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel/include:$CPATH"
 export PWD="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/build"
-export PYTHONPATH="/home/robotics/git_work/cola-blimp/jtradwil/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
