8b0dc15ee68099248cbc7e9512dd8d6a93a47905
jtradwil <jtradwil@mtu.edu>
2017-01-16 12:44:18 -0500

    Modified publisher script to use custom messages

--- lab_one_ws/src/weather_pkg/scripts/weather_pub.py
+++ lab_one_ws/src/weather_pkg/scripts/weather_pub.py
@@ -3,9 +3,10 @@ import sys
 import rospy
 import pyowm
 from std_msgs.msg import Int64
+from weather_pkg.msg import weather
 
 def weather_pub(x):
-    pub = rospy.Publisher('chatter', Int64, queue_size=10)
+    pub = rospy.Publisher('chatter', weather, queue_size=10)
     rospy.init_node('weather_pub', anonymous=True)
     rate = rospy.Rate(0.1) # 10hz
     owm = pyowm.OWM('211a0e00ae519d723c94311c4ec86126')
@@ -14,9 +15,15 @@ def weather_pub(x):
 	city = owm.weather_at_zip_code(x,"us")
 	w=city.get_weather()
 	temp=w.get_temperature('fahrenheit')
-	temp =temp['temp']
-	rospy.loginfo(temp)
-        pub.publish(temp)
+	pressure=w.get_pressure()
+	msg=weather()
+	msg.zipcode=x
+	msg.temp=temp['temp']
+	msg.cloud_cover=w.get_clouds()
+	msg.pressure=pressure['press']
+	msg.dew_point=w.get_dewpoint()
+	rospy.loginfo(msg)
+        pub.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
