beef4bf8e9f90ac33e651653f5d68825eeb7c891
jonschulz86 <jschulz1@mtu.edu>
2017-01-16 10:06:38 -0500

    added code for Extra #1. First argument when running weather_pub.py will be the zip code. It not argument is given defaulys to Houghton.

--- lab_one_ws/src/weather_pkg/scripts/weather_pub.py
+++ lab_one_ws/src/weather_pkg/scripts/weather_pub.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+import sys
 import rospy
-from std_msgs.msg import Int64
 import pyowm
+import myCode
 
 
-def weather_pub():
+def weather_pub(x):
     pub = rospy.Publisher('chatter', Int64, queue_size=10)
     rospy.init_node('weather_pub', anonymous=True)
     rate = rospy.Rate(0.1) # 10hz
     owm = pyowm.OWM('211a0e00ae519d723c94311c4ec86126')
    
     while not rospy.is_shutdown():
-	city = owm.weather_at_zip_code("49931","us")
+	city = owm.weather_at_zip_code(x,"us")
 	w=city.get_weather()
 	temp=w.get_temperature('fahrenheit')
 	temp =temp['temp']
@@ -20,7 +21,16 @@ def weather_pub():
         rate.sleep()
 
 if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        x = sys.argv[1]
+
+    else:
+        x = "49931"  #default to Houghton if no zip code given
+
     try:
-        weather_pub()
+        weather_pub(x)
+
     except rospy.ROSInterruptException:
         pass
+
